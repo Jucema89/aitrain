f9a93fb9b6ad6c759589e36449b4fdaa04255ce8
Julio Daza <jucema89@gmail.com>
2024-06-21 16:13:04 -0500

    Upgrade V18 Finish

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClient, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appInterceptor } from './services/interceptor/app.interceptor';
 import { SvgIconRegistryService } from 'angular-svg-icon';
 import { ApiService } from './services/api/api.service';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser/index';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(
+    provideHttpClient (
       withInterceptors([appInterceptor]),
     ),
     SvgIconRegistryService
